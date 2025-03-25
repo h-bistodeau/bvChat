37196a564010b531ae7bcb50a5c182aeab3b7922
Hailey Bistodeau <120686028+h-bistodeau@users.noreply.github.com>
2025-03-25 10:48:56 -0500

    Update bvChat-Client.py

--- bvChat-Client.py
+++ bvChat-Client.py
@@ -1,2 +1,69 @@
-# client side file
-# we chose additional feature 3 the spam blocker
+import threading
+from socket import *
+
+clientSocket = socket(AF_INET, SOCK_STREAM)
+running = True
+
+def recv_message(client_socket):
+    global running
+
+    while running:
+        try:
+            msg = client_socket.recv(1024)
+
+            # if no message has actually been recieved exit the loop
+            if not msg:
+                print("Connection closed by someone methinks")
+                break
+
+            # print whatever message has been recieved
+            print(msg.decode())
+
+        except ConnectionResetError:
+            print("lost connection with the server")
+            break
+
+    client_socket.close()
+    running = False # ideally this should stop whatever thread this is running on
+
+
+
+def handle_client(client_socket):
+    global running
+
+    # prompt the user for the IP and Port numbers
+    server_info =  input("enter the IP and port of the server: ")
+    serverIP, serverPort = server_info.split(" ")
+
+    try:
+        #attempt to connect to the server
+        client_socket.connect((serverIP, int(serverPort)))
+        print("Connected to server!")
+
+    except ConnectionRefusedError:
+        print("Connection to server was refused please make sure you used the correct IP and port")
+        return
+
+    # create a thread that listens for incoming messages via the server
+    threading.Thread(target=recv_message, args=(client_socket,), daemon=True).start()
+
+    while running:
+        try:
+            message = input()
+            if message.lower() == "/exit":
+                client_socket.send(message.encode())
+                break
+
+            client_socket.send(message.encode())
+
+        except ConnectionResetError:
+            print(" I've lost connection so sorry <3 -your server")
+            break
+
+    running = False
+    client_socket.close()
+    print("Connection closed")
+
+
+handle_client(clientSocket)
+
